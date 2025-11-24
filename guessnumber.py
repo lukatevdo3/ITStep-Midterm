a385fe7db9258ddc14d1596916d6582694f9fd0c
lukatevdo3 <lukatevdo3@gmail.com>
2025-11-24 20:46:06 +0400

    new

---
+++ guessnumber.py
@@ -0,0 +1,31 @@
+# Guess the number Game
+
+import random
+
+
+num = random.randint(1,101)
+count = 1
+print("გამოიცანით რიცხვი 1-დან 100-მდე")
+while True:
+    try:
+        guess = int(input(f"რიცხვი (მცდელობა {count}): "))
+        if guess < 0:
+            print("რიცხვები უნდა იყოს მხოლოდ 1-დან 100-მდე!!")
+        else:
+            if guess > num:
+                print("თქვენი რიცხვი მეტია გამოსაცნობ რიცხვზე!!")
+            elif guess < num:
+                print("თქვენი რიცხვი ნაკლებია გამოსაცნობ რიცხვზე!!")
+            else:
+                print("ყოჩაღ! თქვენ გამოიცანით რიცხვი!")
+                break
+
+ 
+        
+        count += 1
+    except ValueError:
+        print("შეიყვანეთ რიცხვი!!")
+
+
+
+        
